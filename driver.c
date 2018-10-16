e903ae2a4e0a01e88dff40be5628ec36a8c1bd21
Crystal Wang <cwang4@stuy.edu>
2018-10-16 10:52:05 -0400

    Wrote basic main skeleton

--- driver.c
+++ driver.c
@@ -19,7 +19,91 @@ int main(){
   print_list(ll);
 
   struct song_node *target;
-  target = find_node(ll, "qwer", "n0, u");
+  // target = find_node(ll, "qwer", "n0, u");
+
+  printf("LINKED LIST TESTS\n====================================\n\nTesting print_list:\n");
+  printf("REPLACE LATER\n");
+  printf("====================================\n\n");
+  printf("Testing print_node\n");
+  printf("REPLACE LATER\n");
+  
+  printf("====================================\n\n");
+  printf("Testing find_node\n");
+  printf("REPLACE LATER\n");
+
+  printf("====================================\n\n");
+  printf("Testing find_artist\n");
+  printf("REPLACE LATER\n");
+
+  printf("====================================\n\n");
+  printf("Testing songcmp\n");
+  printf("REPLACE LATER\n");
+
+  printf("====================================\n\n");
+  printf("Testing random\n");
+  printf("REPLACE LATER\n");
+
+  printf("====================================\n\n");
+  printf("Testing remove\n");
+  printf("REPLACE LATER\n");
+
+  printf("====================================\n\n");
+  printf("Testing remove\n");
+  printf("REPLACE LATER\n");
+
+  printf("====================================\n\n");
+  printf("Testing free_list\n");
+  printf("REPLACE LATER\n");
+
+  printf("====================================\n\n");
+  printf("MUSIC LIBRARY TESTS\n");
+
+  printf("====================================\n\n");
+  printf("Testing print_library\n");
+  printf("REPLACE LATER\n");
+
+  printf("====================================\n\n");
+  printf("Testing print_letter\n");
+  printf("REPLACE LATER\n");
+
+  printf("====================================\n\n");
+  printf("Testing find\n");
+  printf("REPLACE LATER\n");
+
+  printf("====================================\n\n");
+  printf("Testing find_artist\n");
+  printf("REPLACE LATER\n");
+
+  printf("====================================\n\n");
+  printf("Testing remove_song\n");
+  printf("REPLACE LATER\n");
+
+  printf("====================================\n\n");
+  printf("Testing clear_library\n");
+  printf("REPLACE LATER\n");
+
+  printf("====================================\n\n");
+  printf("Adding songs to empty library\n");
+  printf("REPLACE LATER\n");
+
+  printf("====================================\n\n");
+  printf("Testing print_arist\n");
+  printf("REPLACE LATER\n");
+
+  printf("====================================\n\n");
+  printf("Testing shuffle");
+  printf("REPLACE LATER\n");
+
+  printf("====================================\n\n");
+
+  
+
+  printf("\n");
+  printf("\n");
+  printf("\n");
+  printf("\n");
+  printf("\n");
+  printf("\n");
 
   return 0;
 }
