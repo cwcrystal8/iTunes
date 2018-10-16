fa4672b043397937143097b451e52bd0aa1b839d
William Lu <rlu1@stuy.edu>
2018-10-16 10:42:16 -0400

    p

--- linkedlist.h
+++ linkedlist.h
@@ -1,3 +1,6 @@
+#ifndef LINKEDLIST_H
+#define LINKEDLIST_H
+
 struct song_node {
   char name[100];
   char artist[100];
@@ -29,3 +32,5 @@ struct song_node *get_random(struct song_node *head);
 struct song_node *remove_node(struct song_node *head, char *name, char *artist);
 
 struct song_node *free_list(struct song_node *head);
+
+#endif
